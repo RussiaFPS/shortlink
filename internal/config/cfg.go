3e880b0bb10b16dba3d2d25667de63ad1463c28c
nikita <nikitakrut58@gmail.com>
2025-08-29 00:59:54 +0300

    test cfg

--- internal/config/cfg.go
+++ internal/config/cfg.go
@@ -7,9 +7,9 @@ import (
 )
 
 type Config struct {
-	ServerAddr string `env:"SERVER_ADDRESS,required"`
-	BaseURL    string `env:"BASE_URL,required"`
-	//FileStoragePath string `env:"FILE_STORAGE_PATH,required"`
+	ServerAddr      string `env:"SERVER_ADDRESS,required"`
+	BaseURL         string `env:"BASE_URL,required"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH,required"`
 }
 
 var (
@@ -25,15 +25,15 @@ func NewConfig() *Config {
 			return
 		}
 
+		if cfg.FileStoragePath == "" {
+			flag.StringVar(&cfg.FileStoragePath, "f", "storage.json", "File storage path")
+		}
 		if cfg.ServerAddr == "" {
 			flag.StringVar(&cfg.ServerAddr, "a", "localhost:8080", "HTTP server address")
 		}
 		if cfg.BaseURL == "" {
 			flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080", "Base URL for shortened links")
 		}
-		//if cfg.FileStoragePath == "" {
-		//	flag.StringVar(&cfg.FileStoragePath, "f", "storage.json", "File storage path")
-		//}
 
 		flag.Parse()
 	})
