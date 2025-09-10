3394a00922f695afd8a0ace9319c0de748ff387c
nikita <nikitakrut58@gmail.com>
2025-09-10 22:49:23 +0300

    fix multi add urls

--- internal/repository/database/query.go
+++ internal/repository/database/query.go
@@ -21,7 +21,7 @@ const findLongURL = `
 const createTable = `
 	CREATE TABLE IF NOT EXISTS urls (
                         id SERIAL PRIMARY KEY,
-                        short_url VARCHAR(20) NOT NULL,
+                        short_url VARCHAR(255) NOT NULL,
                         long_url  VARCHAR(255) NOT NULL,
                         CONSTRAINT unique_long_url UNIQUE (long_url),
                         CONSTRAINT unique_short_url UNIQUE (short_url)
